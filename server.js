92b11aebfd3d6e887eda3a36c52cf6f7e932d7c6
Abrar Chowdhry <abrarchowdhry44@hotmail.com>
2021-11-23 16:16:53 -0500

    Updated folders, added routes and mongodb connect

---
+++ server.js
@@ -0,0 +1,32 @@
+// Import Express
+const express = require('express');
+// Import Mongoose
+const mongoose = require('mongoose');
+
+
+// Port Used
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { User, Thought } = require('./models');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Listen with mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cluster0', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('useCreateIndex', true);
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+
+// Listen for connection
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
